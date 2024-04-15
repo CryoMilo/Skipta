075b4333f773602cf38d519ea746dbadc4bb4b65
CryoMilo <minkojee@gmail.com>
2024-04-15 13:41:47 +0700

    added redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import profileSlice from "../features/profile/profileSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const rootReducer = combineReducers({
+	profile: profileSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: {
-		profile: profileSlice,
-	},
+	reducer: persistedReducer,
 });
--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,17 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
