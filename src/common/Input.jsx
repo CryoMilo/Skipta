31380662818e0c268d09a15df0b7276d1fca9e55
CryoMilo <minkojee@gmail.com>
2024-04-18 21:31:49 +0700

    added vouncher manipulation

--- src/common/Input.jsx
+++ src/common/Input.jsx
@@ -21,7 +21,7 @@ const Input = ({
 					type={type}
 					defaultValue={defaultValue}
 					placeholder={placeholder}
-					className={`input text-center w-full max-w-xs border-b-2 border-b-accent rounded-none ${className}`}
+					className={`input text-center w-full max-w-xs border-b-2 border-b-secondary rounded-none ${className}`}
 				/>
 			)}
 		/>
--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -2,10 +2,23 @@ import { useSelector } from "react-redux";
 import Input from "../common/Input";
 import { useForm } from "react-hook-form";
 import { TrashIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const Bill = ({ bill }) => {
 	const profiles = useSelector((state) => state.profile);
+
+	const payerRemovedList = profiles.data.filter(
+		(item) => item.id !== bill.payer.id
+	);
+
+	const [payeeList, setPayeelist] = useState(payerRemovedList);
+
 	const { control, handleSubmit } = useForm();
+
+	const removePayeeList = (idToRemove) => {
+		const newList = payeeList?.filter((item) => item.id !== idToRemove);
+		setPayeelist(newList);
+	};
 
 	const onSubmit = (data) => {
 		console.log(data);
@@ -33,27 +46,26 @@ const Bill = ({ bill }) => {
 					<div>{bill.payer.username}</div>
 					<Input
 						name={`${bill.payer.username}_cost_for_${bill.id}`}
-						disabled
 						defaultValue={bill.amount}
 						control={control}
 					/>
 				</div>
-				{profiles?.data.map((item) => {
-					if (item.id !== bill.payer.id) {
-						return (
-							<div
-								key={item.id}
-								className="border-none grid grid-cols-[0.1fr_2fr_0.5fr] items-center my-4">
-								<TrashIcon className="w-4 h-4 text-red-400" />
-								<div>{item.username}</div>
-								<Input
-									name={`${item.username}_cost_for_${bill.id}`}
-									control={control}
-								/>
-							</div>
-						);
-					}
-				})}
+				{payeeList.map((payee) => (
+					<div
+						key={payee.id}
+						className="border-none grid grid-cols-[0.1fr_2fr_0.5fr] items-center my-4">
+						<div
+							onClick={() => removePayeeList(payee.id)}
+							className="cursor-pointer">
+							<TrashIcon className="w-4 h-4 text-red-400" />
+						</div>
+						<div>{payee.username}</div>
+						<Input
+							name={`${payee.username}_cost_for_${bill.id}`}
+							control={control}
+						/>
+					</div>
+				))}
 			</div>
 		</form>
 	);
