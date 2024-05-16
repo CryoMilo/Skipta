c560fa928a79938bdb17a02531d74250430566b2
CryoMilo <minkojee@gmail.com>
2024-05-16 17:41:55 +0700

    removed new profile qr errors

--- src/pages/Settle.jsx
+++ src/pages/Settle.jsx
@@ -3,10 +3,10 @@ import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
 import { settlePayments } from "../utils/settlePayments";
 import { ArrowLongRightIcon } from "@heroicons/react/20/solid";
-import ReceiverBankQRCode from "../components/ReceiverBankQRCode";
 
 const Settle = () => {
 	const bills = useSelector((state) => state.bill);
+	const profiles = useSelector((state) => state.profile);
 
 	const billDataCopy = JSON.parse(JSON.stringify(bills.data));
 
@@ -21,6 +21,10 @@ const Settle = () => {
 			<Navbar />
 			<section className="my-10 max-w-[600px] mx-auto px-10">
 				{transactions?.map(({ payer, receiver, cost }, index) => {
+					const receiverProfile = profiles.data?.filter(
+						(item) => item.username === receiver
+					);
+
 					return (
 						<div
 							key={index}
@@ -30,7 +34,7 @@ const Settle = () => {
 								<div className="grid grid-cols-3 justify-items-center">
 									{payer} <ArrowLongRightIcon className="w-5" /> {receiver}
 								</div>
-								<div className="text-center">{cost} THB</div>
+								<div className="text-center">{cost.toFixed(1)} THB</div>
 								<p className="text-center pt-3 text-sm select-none">
 									(Click to pay)
 								</p>
@@ -41,20 +45,26 @@ const Settle = () => {
 										<tbody className="text-center">
 											<tr className="border-none">
 												<th>Account Number</th>
-												<td>{bills.data[0].payer.bankAccountNumber}</td>
+												<td>{receiverProfile[0].bankAccountNumber}</td>
 											</tr>
 											<tr className="border-none">
 												<th>Account Name</th>
-												<td>{bills.data[0].payer.bankAccountName}</td>
+												<td>{receiverProfile[0].bankAccountName}</td>
 											</tr>
 											<tr className="border-none">
 												<th>Bank Name</th>
-												<td>{bills.data[0].payer.bankName}</td>
+												<td>{receiverProfile[0].bankName}</td>
 											</tr>
 										</tbody>
 									</table>
 								</div>
-								<ReceiverBankQRCode receiver={receiver} />
+								{receiverProfile.length !== 0 && (
+									<img
+										className="rounded-lg"
+										src={receiverProfile[0]?.bankQRCode}
+										alt="Payment QR"
+									/>
+								)}
 							</div>
 						</div>
 					);
